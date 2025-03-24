99f1506a31d08b43186f63f10b9fd9b2b47ae008
vbacho <victorbachour@gmail.com>
2025-03-24 14:51:00 -0700

    imports are now in

---
+++ titanic.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+data = pd.read_csv('Titanic-Dataset.csv')
