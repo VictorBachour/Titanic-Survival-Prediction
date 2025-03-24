d2b6463b35383e77e467e238c186cb9762ba9a8f
vbacho <victorbachour@gmail.com>
2025-03-24 15:25:31 -0700

    dataset has a 0.8100558659217877 accuracy to guess correctly

--- titanic.py
+++ titanic.py
@@ -11,14 +11,22 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 data = pd.read_csv('Titanic-Dataset.csv')
 data['Age'] = data['Age'].fillna(data['Age'].median())
 data['Embarked'] = data['Embarked'].fillna(data['Embarked'].mode()[0])
-data.drop(columns='Cabin', inplace=True)
+data.drop(columns=['Cabin','Ticket','Name'], inplace=True)
 
-x = data.drop(columns=['Survived', 'Name'])
+le = LabelEncoder()
+data['Sex'] = le.fit_transform(data['Sex'])
+data['Embarked'] = le.fit_transform(data['Embarked'])
+
+x = data.drop(columns=['Survived'])
 y = data['Survived']
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size= .2, random_state=42)
-model = LogisticRegression(solver='lbfgs', max_iter=200, C=1.0, penalty='l2')
+model = LogisticRegression(solver='lbfgs', max_iter=1000, C=1.0, penalty='l2')
+
 model.fit(X_train, y_train)
 
-y_pred = model.predict(y_test)
+y_pred = model.predict(X_test)
 print(y_pred)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
