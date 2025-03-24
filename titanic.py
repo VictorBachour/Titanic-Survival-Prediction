803a71d3a70f86a1dd3f3340535363f6d7f617aa
vbacho <victorbachour@gmail.com>
2025-03-24 15:33:05 -0700

    best one was RFC for accuracy at .8268156424581006

--- titanic.py
+++ titanic.py
@@ -1,11 +1,9 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+import xgboost as xgb
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 data = pd.read_csv('Titanic-Dataset.csv')
@@ -20,9 +18,14 @@ data['Embarked'] = le.fit_transform(data['Embarked'])
 x = data.drop(columns=['Survived'])
 y = data['Survived']
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size= .2, random_state=42)
-model = LogisticRegression(solver='lbfgs', max_iter=1000, C=1.0, penalty='l2')
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
 
+X_train, X_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
+
+#model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+#model = LogisticRegression(solver='lbfgs', max_iter=1000, C=1.0, penalty='l2')
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
